a4afab75d5dbf81f0e38b1186b25e09ff1e1ff7b
sarwin_ <64829298+SarwinR@users.noreply.github.com>
2024-03-10 16:32:19 +0400

    Add stopwatch timer and back button to ActiveWorkout screen

--- lib/screens/workouts/active_workout.dart
+++ lib/screens/workouts/active_workout.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:gym_buddy_app/models/workout.dart';
 import 'package:gym_buddy_app/screens/ats_ui_elements/ats_button.dart';
+import 'package:gym_buddy_app/screens/ats_ui_elements/ats_icon_button.dart';
 import 'package:gym_buddy_app/screens/workouts/widgets/exercises_rep_set_display.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 
 class ActiveWorkout extends StatefulWidget {
-  const ActiveWorkout({super.key, required this.workoutTemplate});
+  ActiveWorkout({super.key, required this.workoutTemplate});
 
   final Workout workoutTemplate;
+
+  final stopWatchTimer = StopWatchTimer(mode: StopWatchMode.countUp);
+
+  String displayTime = "";
 
   @override
   State<ActiveWorkout> createState() => _ActiveWorkoutState();
@@ -14,14 +20,43 @@ class ActiveWorkout extends StatefulWidget {
 
 class _ActiveWorkoutState extends State<ActiveWorkout> {
   @override
+  void initState() {
+    super.initState();
+    widget.stopWatchTimer.onStartTimer();
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await widget.stopWatchTimer.dispose(); // Need to call dispose function.
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.workoutTemplate.name),
+        leading: atsIconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              widget.stopWatchTimer.onStartTimer();
+              Navigator.pop(context);
+            }),
         actions: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: atsButton(child: Text("1h 21m 30s"), onPressed: () {}),
+            child: atsButton(
+                child: StreamBuilder<int>(
+                  stream: widget.stopWatchTimer.rawTime,
+                  initialData: 0,
+                  builder: (context, snapshot) {
+                    final value = snapshot.data;
+                    final displayTime = StopWatchTimer.getDisplayTime(value!,
+                        milliSecond: false);
+                    return Text(displayTime);
+                  },
+                ),
+                onPressed: () {}),
           )
         ],
       ),
