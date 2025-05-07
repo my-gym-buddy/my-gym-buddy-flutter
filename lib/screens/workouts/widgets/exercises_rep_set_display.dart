2dfdd0e64fe2ac3b8a78e383896c445b51872ed1
levynKwong <levynpersonal@gmail.com>
2025-05-07 12:39:45 +0400

    removed an unwanted import that causes the app to crash

--- lib/screens/workouts/widgets/exercises_rep_set_display.dart
+++ lib/screens/workouts/widgets/exercises_rep_set_display.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/material.dart';
 import 'package:gym_buddy_app/models/exercise.dart';
 import 'package:gym_buddy_app/models/rep_set.dart';
